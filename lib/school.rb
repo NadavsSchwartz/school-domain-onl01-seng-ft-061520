0a8dd40c8fffce08ddec6d08b42ffd8f22073982
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 03:45:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,12 +12,13 @@ class School
     roster[grade] << name
   end
   
-    def grade(grade)
-    roster.detect do |x, y| 
-      if x == grade
-        return y 
-      end 
-    end 
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
   end 
 
 end
