01697506dfc87e92fa9ca6f63e0d8f14d819a125
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 03:43:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,9 @@ class School
     roster[grade] << name
   end
   
-    def grade(level)
+    def grade(grade)
     roster.detect do |x, y| 
-      if x == level
+      if x == grade
         return y 
       end 
     end 
