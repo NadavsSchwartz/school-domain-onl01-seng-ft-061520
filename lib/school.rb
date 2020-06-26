ce0c6770e957563719e88d682a4e7450d4b12fa5
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 03:32:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 
 class School
 
-	attr_accessor :roster, :name
+	attr_accessor :roster, :student_name, :student_grade
 	attr_reader :school 
 	
 	def initialize(school)
@@ -13,5 +13,5 @@ class School
   end 
   
   def add_student(student_name, student_grade)
-
+      roster[student_grade]
 end
