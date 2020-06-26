ffbeab904ec2e65fbfa1af37040616e813a8ebb4
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-26 03:24:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  attr_accessor :model, :roster
+  attr_reader :model
+  
+  def initiallize(model)
+    @model = model
+  end
+  
+    def roster
+      roster = {}
+  end
+
+
+end
